564a5c75eb1f8960149eb445ee9745321b8bd85c
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-05-28 04:10:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,18 +11,27 @@ int main() {
 	int position; //position of book for its deletion
 
 	//Load file and read item in file. Work in background without user knowing
-	ifstream infile;
-	ofstream outfile;
+	//ifstream infile;
+	//ofstream outfile;
 	string next, next2, next3;
 	List<string> list;
-	infile.open("infile.txt");
-	outfile.open("outfile.txt");
+
+	//infile.open("infile.txt");
+	fstream fs;
+	fs.open ("infile.txt", fstream::in);
 
 	//load the data into Linked List.
+	/*
 	while (getline(infile, next), getline(infile, next2), getline(infile, next3)) {
+		list.addBook(next, next2, next3);
+	} */
+
+	while (getline(fs, next), getline(fs, next2), getline(fs, next3)) {
 		list.addBook(next, next2, next3);
 	}
 
+	fs.close();
+	
 	//Menu.
 	cout << "\nLibrary Book Management System" << endl;
 	cout << "Version 001.001\n\n\n";
@@ -42,7 +51,7 @@ int main() {
 				cout << "1. Display book." << endl;
 				cout << "2. Add book." << endl;
 				cout << "3. Delete book." << endl;
-				//cout << "4. Update book." << endl;
+				cout << "4. Update book." << endl;
 				//cout << "5. Search book." << endl;
 				//cout << "6. Sort book." << endl;
 				cout << "7. Save." << endl;
@@ -60,6 +69,7 @@ int main() {
 				case 3: 
 					cout << "Enter book position." << endl;
 					cin >> position;						
+					
 					if (position == 1) { //if position = 1 - i.e. first node (head) - delete head
 						list.deleteHead();
 					}
@@ -69,12 +79,24 @@ int main() {
 					break;
 				case 4: 
 					cout << "Update book: " << endl;
-
+					cout << "Enter book position: " << endl;
+					cin >> position;
+					list.updateBook(position);
+				case 7:
+					fs.open("infile.txt", fstream::out);
+					list.temp = list.head;
+					while (list.temp != NULL) {
+						fs << list.temp->bookID << endl;
+						fs << list.temp->bookTitle << endl;
+						fs << list.temp->author << endl;
+						list.temp = list.temp->next;											
+					}
+					break;
 				case 8: //quitting condition
 					ans = 8;
 					break;
 				} 
-			} while (ans != 3);
+			} while (ans != 8);
 			break;
 		case 2:
 			cout << "Documentation" << endl;
@@ -88,18 +110,17 @@ int main() {
 	
 		//write the file out. Also works in background. Will make a save feature later.
 		//Also, save automatically if the user exit (normal exit).
+		/*
 		list.temp = list.head;
 		while (list.temp != NULL) {
 			outfile << list.temp->bookID << endl;
 			outfile << list.temp->bookTitle << endl;
 			outfile << list.temp->author << endl;
 			list.temp = list.temp->next;
-		}
-
-
-		infile.close();
-		outfile.close();
-
-		return 0;
+		} 
+		*/
+	fs.close();
+	
+	return 0;
 }
 
