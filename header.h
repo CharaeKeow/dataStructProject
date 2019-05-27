79de7e095cef9122555c3ffc1377117b3546eb42
CharaeKeow <34004186+CharaeKeow@users.noreply.github.com>
2019-05-27 23:00:13 +0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,26 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+template <class T>
+class List {
+private:
+	struct Node {
+		T bookID;
+		T bookTitle;
+		T author;
+		Node* next;
+	};
+public:
+	Node* head, * tail, *temp;
+	List();
+	~List() {};
+	void addBook(T bookID, T bookTitle, T author);
+	void displayBook();
+	void deleteBook(int position);
+	void deleteHead();
+	void updateBook(int position);
+
+	void addBookInput();
+};
+
+#endif
